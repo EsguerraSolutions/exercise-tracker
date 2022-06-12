b2c7986b144bf49d372a488a536c7f69c9fc2503
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-06-13 05:36:58 +0800

    15th commit

--- index.js
+++ index.js
@@ -68,21 +68,21 @@ app.get('/api/users/:_id/logs', async (req,res) => {
   const id = req.params['_id'];
   const userInfo = await usersDB.findOne({_id : id},'-__v');
   const {from , to , limit} = req.query;
-  // const gte = new Date(from);
-  // const lte = new Date(to);
+  const gte = new Date(from);
+  const lte = new Date(to);
   let filter = {};
-  // if (gte != 'Invalid Date' && lte != 'Invalid Date') {
-  //   filter = { 
-  //     userID : id,
-  //     date: {
-  //       $gte: gte,
-  //       $lte : lte
-  //     }}
-  // }
+  if (gte != 'Invalid Date' && lte != 'Invalid Date') {
+    filter = { 
+      userID : id,
+      date: {
+        $gte: gte,
+        $lte : lte
+      }}
+  }
 
-  // else {
+  else {
     filter = { userID : id }
-  // }
+  }
   const userLogsData = await logsDB.find(filter, 'description duration date -_id').limit(limit);
   const userLogs = {
     username : userInfo.username,
@@ -90,6 +90,7 @@ app.get('/api/users/:_id/logs', async (req,res) => {
     '_id' : userInfo._id,
     log : userLogsData
   };
+  console.log(userLogs);
   return res.json(userLogs);
 });
 
